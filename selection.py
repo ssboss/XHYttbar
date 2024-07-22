56a15fa872537f79bc3f5852db41c2abc747944a
Emmanuel Moses <emoses@cmslpc250.fnal.gov>
2024-07-22 12:19:54 -0500

    Adding Initial Snapshot File

---
+++ selection.py
@@ -0,0 +1,8 @@
+# basic script to analyze 
+
+import ROOT, time
+from TIMBER.Analyzer import HistGroup, Correction
+from TIMBER.Tools.Common import CompileCpp, ExecuteCmd
+from collections import OrderedDict
+import TIMBER.Tools.AutoJME as AutoJME
+
